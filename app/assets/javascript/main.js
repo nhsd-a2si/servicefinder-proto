770eb4d0de4280dbe008f05885be9cb096b406a7
paulpod <paulpod@neuromantics.net>
2019-05-30 14:20:45 +0100

    replaced the autocomplete listjs with something more specific, hides none selected peers in nested list of synonyms, and shows parent list items with checkbox as required for usage

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,38 @@
 // ES6 or Vanilla JavaScript
+function servicetypesearch() {
+
+$('#nhsuk-footer').css("display","none");
+
+
+var labels = $('#service-list-container label');  // cache this for better performance
+
+$('input#servicetype').keypress(function() {
+      $('#service-list-container').css("visibility","visible");
+      $('html').css("height","100%");
+      $(this).focus();
+    });
+
+$('#servicetype').keyup(function() {
+  var valThis = $(this).val().toLowerCase();
+
+  
+  if (valThis == "") {
+    labels.parent().hide();          // show all lis
+  } else {
+    labels.each(function() {
+      var label = $(this);                    // cache this
+      var text = label.text().toLowerCase();
+      if (text.indexOf(valThis) > -1) {
+      	//label.addClass('vis');
+        label.parents('li').show()           // show all li parents up the ancestor tree
+      } else {
+       //label.removeClass('vis');
+       label.parent().hide();                // hide current li as it doesn't match
+      }
+    });
+  };
+});
+
+
+ 
+};
--- app/routes.js
+++ app/routes.js
@@ -5,3 +5,73 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+
+router.get('/login', function (req, res) {
+  var compare = req.query.username
+
+  // get the number of times the user since login has done a search
+  global.run = req.query.run
+
+  // and add one
+  console.log("run:" + run )
+
+  res.render('servicefinder-searchtype', { run:run })
+})
+
+
+router.get('/searchtype', function (req, res) {
+  var run = req.query.run
+
+  // and add one
+  run = parseInt(run) + 1
+  console.log("run:" + run )
+
+  res.render('symptom-search', { run:run })
+})
+
+
+// service type select ... grabbing picks to playback later
+router.get('/services', function (req, res) {
+  var run = req.query.run
+  console.log("run:" + run )
+
+  var compare = req.query.selectedservices
+  var selectedservices = req.query.selectedservices
+
+  console.log(selectedservices.length)
+  console.log(selectedservices)
+
+
+  if (selectedservices.includes('Minor Injury Unit (MIU)')) {
+    // Yes, it's there
+    console.log("branch to MIU finder")
+    res.render('servicefinder-selectedservices-miu', { selectedservices:selectedservices, run:run })
+
+  } else if (selectedservices.find(a =>a.includes("Mental"))) {
+    // Yes, it's there
+    console.log("branch to Mental Health finder")
+    res.render('servicefinder-selectedservices-mh', { selectedservices:selectedservices, run:run })
+  } else {
+    // Everything else just checks the selected items if more than one
+
+    if (selectedservices.length >= 2) {
+      // more than one selected so review the selected items
+      res.render('servicefinder-selectedservices', { selectedservices:selectedservices, run:run })
+    } else {
+      // or go straight to the next step (location) if only one type is selected...
+      res.render('servicefinder-location-field', { selectedservices:selectedservices, run:run })
+    }
+  }
+  
+})
+
+
+
+router.get('/selected', function (req, res) {
+  var selectedservices = req.query.selectedservices
+  var run = req.query.run
+  console.log("run:" + run )
+
+  res.render('servicefinder-location-field', {  selectedservices:selectedservices, run:run })
+})
